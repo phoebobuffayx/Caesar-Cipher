e256cda82a986cee6f970d399a926ffa77401b67
phoebobuffayx <161206357+phoebobuffayx@users.noreply.github.com>
2024-03-20 14:29:12 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,46 @@
+#main
+import cypher_art
+import os
+import alphabet_list
+
+alphabet = alphabet_list.alphabet
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+
+should_end = False
+while not should_end:
+    print(cypher_art.logo)
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
+
+    clear_screen()  
+    
+
+
